124faa7701c718aeb0592c05e4dcedaa07f2d812
santeolinfo <philippe.lett@santeol.com>
2025-08-19 14:41:49 +0200

    commit 2

--- cleanDownload.ps1
+++ cleanDownload.ps1
@@ -1,6 +1,6 @@
 $DownloadPath = "$env:USERPROFILE\Downloads"
 
-# Vérifier si le dossier existe
+# Vérifier si le dossier existe bien
 if (Test-Path $DownloadPath) {
     Remove-Item -Path "$DownloadPath\*" -Force -Recurse
 }
